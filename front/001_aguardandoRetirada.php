56fce3e61ad5e9b0c1c31cfa674077172eb92ffe
Jeremias Regiani <jeremias.farizeli@gmail.com>
2022-07-22 07:41:22 -0300

    Atualizado página aguardando retirada.

--- front/001_aguardandoRetirada.php
+++ front/001_aguardandoRetirada.php
@@ -19,22 +19,6 @@
 
                     <div>
 
-
-                        <div class="dxc-poisition-right">
-                            <button class="btnGerar" class="btnGerar-info"
-                                onclick="dxcFunctionAguardandoRetirada()">GERAR</button>
-                        </div>
-
-
-
-                    </div>
-
-
-
-
-                    <br><br><br><br><br><br>
-
-                    <div>
                         <textarea class="texto2" cols="30"
                             rows="10">Feita a manutenção e usuário já ciente que deve retirar o equipamento.</textarea>
                     </div>
@@ -49,7 +33,8 @@
                     <div style="width: 300px;"><br>
                         <label class="dxc-label3">Código Equipamento:</label>
                         <label class="copyButton" onclick="copiarTexto()">Copiar</label>
-                        <textarea class="texto1" name="" id="dxc-text-codEquipamento" cols="30" rows="10"></textarea>
+                        <textarea class="texto1" name="" id="dxc-text-codEquipamento" cols="30"
+                            rows="10">AGUARDANDO RETIRADA</textarea>
                     </div>
                     <br><br>
 
@@ -59,7 +44,9 @@
                         <label class="dxc-label3"> Padronização campo anotações:</label>
                         <label class="copyButton" onclick="copiarTextoAgendamentoAnotacoes()">Copiar</label>
                         <textarea class="texto2" name="" id="dxc-text-anotacaoes-aguardandoRetirada" cols="30"
-                            rows="10"></textarea>
+                            rows="10">#AguardandoRetirada
+
+Equipamento reparado, aguardando retirada.</textarea>
                     </div>
 
 
